793463be14b6ffc0be86687155a0f77d2533a940
0rezh <orez.ytb11@gmail.com>
2024-05-18 12:25:28 +0200

    Add: init

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/***',
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'client/***',
+}
+
+server_scripts {
+    'server/***',
+    '@oxmysql/lib/MySQL.lua',
+}
+
+shared_scripts '@es_extended/imports.lua'
